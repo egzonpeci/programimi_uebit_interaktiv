ab1147e0df7c40abc5fca97a62f21ab9bc6d3b60
Egzon Peci <egzon.peci@cacttus.com>
2025-03-17 10:10:29 +0100

    Arry2D

---
+++ js/service_obj.js
@@ -0,0 +1,49 @@
+const ekosova_obj =
+{
+    header: {
+        key_1: "Page",
+        key_2: "Page",
+        key_3: "Page",
+    },
+    lang: {
+        name_sq: "Shq",
+        name_en: "Eng",
+        name_sr: "Srb",
+    },
+    service: [
+        {
+            logo: "icon-x",
+            title: "title_1"
+        },
+        {
+            logo: "icon-x",
+            title: "title_1"
+        },
+        {
+            logo: "icon-x",
+            title: "title_1"
+        },
+        {
+            logo: "icon-x",
+            title: "title_1"
+        },
+    ]
+}
+
+console.log(ekosova_obj.service[0].title);
+
+
+
+
+let students = [
+    ["Student_1", 10, 8, 9, 10, 7, 6, 7, 7],
+    ["Student_2", 10, 8, 9, 10, 7, 6, 7, 7],
+    ["Student_3", 10, 8, 9, 10, 7, 6, 7, 7],
+    ["Student_4", 10, 8, 9, 10, 7, 6, 7, 7],
+    ["Student_5", 10, 8, 9, 10, 7, 6, 7, 7],
+    ["Student_6", 10, 8, 9, 10, 7, 6, 7, 7],
+    ["Student_7", 10, 8, 9, 10, 7, 6, 7, 7]
+];
+
+
+
